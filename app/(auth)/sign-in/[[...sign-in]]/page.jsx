0a8882e08f86dbf44e638949d3f38880f9400336
JC MISA <johncarlomisa399@gmail.com>
2024-07-15 10:21:00 +0800

    minor changes

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -28,7 +28,7 @@ export default function Page() {
             </a>
 
             <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-              Welcome to Squid 🦑
+              Welcome to AI Interview Mocker 🤖
             </h2>
 
             <p className="mt-4 leading-relaxed text-white/90">
--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]); // protects the path with folder dashboard with page.tsx
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect(); // check if use is authenticated when accessing the protected routes above
+  if (isProtectedRoute(req)) auth().protect(); // check if user is authenticated when accessing the protected routes above
 });
 
 export const config = {
